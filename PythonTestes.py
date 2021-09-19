4bcd6a112bc38af9cab2861a8c3a6d96dfdccdfb
Pedro Lopes <pedroplopes09@gmail.com>
2021-09-19 11:50:46 -0300

    soma, subtração e multiplicação de dois números

--- PythonTestes.py
+++ PythonTestes.py
@@ -1,3 +1,8 @@
 print ('teste')
 print ('teste julio')
 val = int(input('digite um número:'))
+val2 = int(input('digite um outro número'))
+s = val + val2
+sub = val - val2
+mult = val * val2
+print ('a soma dos dois números é {}, a subtração é {} e a multiplicação é {}!!'.format(s, sub, mult))
